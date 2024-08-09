d570b2c86c9bfdc6527a7ea1e1e0f5d59fcc19ab
daytomari <tomari.daigorou@link-academy.co.jp>
2024-08-09 13:18:53 +0900

    修正

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,6 +1,5 @@
 package raisetech.StudentManagement;
 
-import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;

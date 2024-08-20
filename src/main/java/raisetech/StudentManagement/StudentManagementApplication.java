04ec5cfc2c59c942eb662072254ba6ea76d50ef5
tomari@home <daytomari@gmail.com>
2024-08-20 11:39:37 +0900

    StudentRepositoryへ@Selectと@Insertを追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,15 +1,20 @@
 package raisetech.StudentManagement;
 
 import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
+
+	@Autowired
+	private StudentRepository repository;
 
 	private String name = "Enami Kouji";
 	private String age = "37";
@@ -20,9 +25,16 @@ public class StudentManagementApplication {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/studentInfo")
-	public String getStudentInfo() {
-		return name + " " + age + "歳";
+	@GetMapping("/student")
+	public String getStudent(@RequestParam String name) {
+		Student student = repository.searchByName(name);
+		return student.getName() + " " + student.getAge() + "歳";
+	}
+
+
+	@PostMapping("/student")
+	public void registerStudent(String name, int age) {
+		repository.registerStudent(name, age);
 	}
 
 	@PostMapping("/studentInfo")

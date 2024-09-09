37c97126f7e2e2414c634fa180ccb9bdaea85487
tomari@home <daytomari@gmail.com>
2024-09-09 21:25:31 +0900

    第22回まで

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -2,9 +2,11 @@ package raisetech.StudentManagement.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
@@ -15,10 +17,12 @@ import raisetech.StudentManagement.service.StudentService;
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
   @GetMapping("/studentList")
@@ -26,21 +30,7 @@ public class StudentController {
     List<Student> students = service.seachStudentList();
     List<StudentsCourses> studentsCourses = service.seachStudentsCourseList();
 
-    List<StudentDetail> studentDetails = new ArrayList<>();
-    for(Student student :students) {
-      StudentDetail studentDetail = new StudentDetail();
-      studentDetail.setStudet(student);
-
-      List<StudentsCourses> convertStudentCourses = new ArrayList<>();
-      for(StudentsCourses studentCourses : studentsCourses) {
-        if(student.getId().equals(studentCourses.getStudentId())) {
-          convertStudentCourses.add(studentCourses);
-        }
-      }
-      studentDetail.setStudentsCourses(convertStudentCourses);
-      studentDetails.add(studentDetail);
-    }
-    return studentDetails;
+    return converter.convertStudentDetails(students, studentsCourses);
   }
 
   @GetMapping("/studentsCourseList")

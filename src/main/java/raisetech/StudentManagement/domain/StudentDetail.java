e2ef6b606225064060b8086ece1aab96e36002f1
Tomari_Home <daytomari@gmail.com>
2025-07-28 23:33:51 +0900

    第42回スワッガー設定まで

--- src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
+++ src/main/java/raisetech/StudentManagement/domain/StudentDetail.java
@@ -1,5 +1,6 @@
 package raisetech.StudentManagement.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import raisetech.StudentManagement.data.StudentCourse;
 
 import java.util.List;
 
+@Schema(description = "受講生詳細")
 @Getter
 @Setter
 @NoArgsConstructor

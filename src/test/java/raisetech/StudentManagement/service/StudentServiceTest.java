596aac4ab59b6c337da8f15a2b53b2c9d3f2f5a8
daytomari <tomari.daigorou@link-academy.co.jp>
2025-07-29 12:20:50 +0900

    課題27　searchStudentのテストを実装

--- src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
+++ src/test/java/raisetech/StudentManagement/service/StudentServiceTest.java
@@ -13,6 +13,7 @@ import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.repository.StudentRepository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,8 +37,9 @@ class StudentServiceTest {
 
     @Test
     void 受講生詳細の一覧検索_リポジトリとコンバーターの処理が適切に呼び出せていること(){ // 元の名前：受講生詳細の一覧検索_全件検索が動作すること
+        // searchStudentListのテスト
+
         // 事前準備
-        StudentService sut = new StudentService(repository, converter);
         List<Student> studentList = new ArrayList<>();
         List<StudentCourse> studentCourseList = new ArrayList<>();
 
@@ -53,7 +55,35 @@ class StudentServiceTest {
         verify(converter, times(1)).convertStudentDetails(studentList, studentCourseList);
 
         // 後処理
-        // ここでDBをもとに戻す
+        // ここでDBをもとに戻すなど
+    }
+
+    @Test
+    void 受講生詳細検索_IDに紐づく受講生情報とコース情報が取得できること(){
+        // 課題27 searchStudentのテスト
+
+        // 事前データ
+        String studentId = "student-001";
+        Student mockStudent = new Student();
+        mockStudent.setId(studentId);
+
+        StudentCourse course1 = new StudentCourse();
+        StudentCourse course2 = new StudentCourse();
+        List<StudentCourse> mockCourseList = Arrays.asList(course1, course2);
+
+        //モック定義
+        when(repository.searchStudent(studentId)).thenReturn(mockStudent);
+        when(repository.searchStudentCourse(studentId)).thenReturn(mockCourseList);
+
+        // 実行
+        StudentDetail result = sut.searchStudent(studentId);
+
+        // 検証
+        verify(repository, times(1)).searchStudent(studentId);
+        verify(repository, times(1)).searchStudentCourse(studentId);
+
+        assertEquals(mockStudent, result.getStudent());
+        assertEquals(mockCourseList, result.getStudentCourseList());
 
     }
 }
